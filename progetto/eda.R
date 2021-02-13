98b976b08f567699a270b6af9e73e653b38fa5ae
Mirco Malanchini <mircomalanchini@hotmai.it>
2021-02-13 22:42:21 +0100

    add ditribution graph

--- progetto/eda.R
+++ progetto/eda.R
@@ -66,6 +66,30 @@ miss_var_summary(combined)
 # Distribuzione Dati White Only
 .plot_class_barplot(whitewine, "quality")
 
+# Distribuzione Dati Red & White Configurazione 1: Multiclasse
+config3 <- preprocess_dataset(combined, 3)
+.plot_class_barplot(config3, "quality")
+
+# Distribuzione Dati Red & White Configurazione 2: 3 Classi
+config2 <- preprocess_dataset(combined, 2)
+.plot_class_barplot(config2, "quality")
+
+# Distribuzione Dati Red & White Configurazione 3: 2 Classi
+config1 <- preprocess_dataset(combined, 1)
+.plot_class_barplot(config1, "quality")
+
+# Distribuzione Dati Red only Configurazione 1: Multiclasse
+config3 <- preprocess_dataset(redwine, 3)
+.plot_class_barplot(config3, "quality")
+
+# Distribuzione Dati Red only Configurazione 2: 3 Classi
+config2 <- preprocess_dataset(redwine, 2)
+.plot_class_barplot(config2, "quality")
+
+# Distribuzione Dati Red only Configurazione 3: 2 Classi
+config1 <- preprocess_dataset(redwine, 1)
+.plot_class_barplot(config1, "quality")
+
 # Distribuzione Dati white only Configurazione 1: Multiclasse
 config3 <- preprocess_dataset(whitewine, 3)
 .plot_class_barplot(config3, "quality")
@@ -77,6 +101,63 @@ config2 <- preprocess_dataset(whitewine, 2)
 # Distribuzione Dati white only Configurazione 3: 2 Classi
 config1 <- preprocess_dataset(whitewine, 1)
 .plot_class_barplot(config1, "quality")
+
+#Attribute Distribution By Class
+#fixed.acidity
+ggplot(combined, aes(x=combined$fixed.acidity, title=fixed.acidity)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#volatile.acidity
+ggplot(combined, aes(x=combined$volatile.acidity, title=volatile.acidity)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#citric.acid
+ggplot(combined, aes(x=combined$citric.acid, title=citric.acid)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+
+#residual.sugar
+ggplot(combined, aes(x=combined$residual.sugar, title=residual.sugar)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#chlorides
+ggplot(combined, aes(x=combined$chlorides, title=chlorides)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#free.sulfur.dioxide
+ggplot(combined, aes(x=combined$free.sulfur.dioxide, title=free.sulfur.dioxide)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#total.sulfur.dioxide
+ggplot(combined, aes(x=combined$total.sulfur.dioxide, title=fixed.aciditytotal.sulfur.dioxide)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#density
+ggplot(combined, aes(x=combined$density, title=density)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#pH
+ggplot(combined, aes(x=combined$pH, title=pH)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#sulphates
+ggplot(combined, aes(x=combined$sulphates, title=sulphates)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
+
+#alcohol
+ggplot(combined, aes(x=combined$alcohol, title=alcohol)) +
+  geom_histogram(color="red") +
+  geom_freqpoly(color="blue")
 
 # Plot boxplot
 .plot_variable_boxplot(config1, "pH", "quality")
