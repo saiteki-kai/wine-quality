e426e218ba3591c7488e498081405e9e6117aec4
Giuseppe <giuseppe.magazu@gmail.com>
2021-02-13 21:29:09 +0100

    add scatter plot for each attribute

--- progetto/outliers.R
+++ progetto/outliers.R
@@ -1,19 +1,21 @@
-# Install packages
-if (!require("pacman")) install.packages("pacman")
-pacman::p_load(corrplot, ggplot2, dplyr, naniar, patchwork, factoextra)
+#' Outliers Analysis
+#'
+#' add description
+#'
 
-.detect_outliers <- function(data) {
-  quantiles <- quantile(data, c(0.25, 0.75), names = FALSE)
-  lower <- quantiles[1] - 1.5 * IQR(data)
-  upper <- quantiles[2] + 1.5 * IQR(data)
-
-  list(lower = lower, upper = upper)
-}
-
-.plot_scatter_outliers <- function(dataset, attribute, hide.outliers = FALSE) {
+#' Plots all points distinguishing outliers from non-outliers
+#' and allows you to hide them through the \code{hide.outliers} parameter.
+#' Outliers are identified through the \code{detect_outliers} function.
+#'
+#' @param dataset a dataset
+#' @param attribute an attribute name
+#' @param label an attribute name indicating the label
+#' @param hide.outliers a logical indicating whether outliers should be hidden
+#' @return the plot
+.plot_scatter <- function(dataset, attribute, label, hide.outliers = FALSE) {
   values <- dataset[[attribute]]
 
-  bounds <- .detect_outliers(values)
+  bounds <- detect_outliers(values)
   is_outlier <- values < bounds$lower | values > bounds$upper
 
   dataset$outlier <- FALSE
@@ -21,7 +23,7 @@ pacman::p_load(corrplot, ggplot2, dplyr, naniar, patchwork, factoextra)
 
   dataset %>%
     filter(!is_outlier | !hide.outliers) %>%
-    ggplot(aes_string(x = "quality", y = attribute)) +
+    ggplot(aes_string(x = label, y = attribute)) +
     geom_jitter(aes(colour = outlier), alpha = 0.3) +
     ggtitle(paste(ifelse(hide.outliers, "Without", "With"), "Outliers")) +
     theme(
@@ -33,7 +35,30 @@ pacman::p_load(corrplot, ggplot2, dplyr, naniar, patchwork, factoextra)
     )
 }
 
-combined <- read.csv("./dataset/winequality-combined.csv")
+# Install packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(ggplot2, dplyr, naniar, patchwork)
 
-.plot_scatter(combined, "residual.sugar", hide.outliers = FALSE) +
-  .plot_scatter(combined, "residual.sugar", hide.outliers = TRUE)
+# Import local functions
+source("./utils.R")
+
+dataset <- read.csv("./dataset/winequality-combined.csv")
+dataset$quality <- factor(dataset$quality) # IMPORTANT
+
+# Print a scatter plot for each attribute with and without outliers
+for (label in c("quality", "type")) {
+  for (attribute in names(dataset)) {
+    if (is.numeric(dataset[[attribute]])) {
+      p <- .plot_scatter(dataset, attribute, label) +
+        .plot_scatter(dataset, attribute, label, hide.outliers = TRUE)
+      print(p)
+    }
+  }
+}
+
+
+# Analyse Outliers
+# dataset <- preprocess_dataset(dataset, 1)
+dataset <- remove_outliers(dataset)
+#gg_miss_var(dataset, quality)
+#gg_miss_fct(dataset, quality)
