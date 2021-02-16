929d5a1326e118fb698920277dae1dc1502cd463
lordfighterx <lordfighterx@gmail.com>
2021-02-16 18:46:04 +0100

    upload models scripts

---
+++ progetto/models/dt.R
@@ -0,0 +1,47 @@
+#' Decision Tree Classification
+#'
+#' add description.
+#'
+
+dt_classification <- function(trainset) {
+  # Install packages
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(caret, klaR, rattle, pROC, dplyr)
+
+  # 10-Fold cross validation
+  tr_control <- trainControl(
+    method = "repeatedcv",
+    number = 10,
+    repeats = 5
+  )
+
+  # Set seed for repeatability
+  set.seed(314)
+
+  # Train the model
+  start_time <- Sys.time()
+  dt_model <- train(
+    quality ~ .,
+    data = trainset,
+    method = "rpart2", # rpart
+    tuneGrid = expand.grid(maxdepth = 2:10),
+    #tuneLength = 10,
+    trControl = tr_control
+  )
+  end_time <- Sys.time()
+  time <- end_time - start_time
+
+  # Print Tuning Process
+  plot(dt_model)
+
+  # Print Tree
+  fancyRpartPlot(dt_model$finalModel)
+
+  # Save the model
+  saveRDS(dt_model, file = "./results/models/dt_model.RDS")
+
+  # Return
+  list(model = dt_model, train_time = time)
+}
+
+
---
+++ progetto/models/svm.R
@@ -0,0 +1,46 @@
+#' Support Vector Machine Classification
+#'
+#' add description.
+#'
+
+svm_classification <- function(trainset) {
+  # Install packages
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(caret)
+
+  # 10-Fold cross validation
+  tr_control <- trainControl(
+    method = "repeatedcv",
+    number = 10,
+    repeats = 5
+  )
+
+  # Set seed for repeatability
+  set.seed(314)
+
+  #tuneLength=10 svmRadial
+  #tuneLength=4 svmPoly
+  grid_radial <- expand.grid(
+    sigma = c(0.01, 0.5, 1), #c(0,0.01,0.1,0.5,0.75,0.9,1),
+    C = c(0.01, 1, 2) #0,0.01,0.1,0.5,0.75,0.9,1,1.5,2,5)
+  )
+
+  # Train the model
+  start_time <- Sys.time()
+  svm_model <- train(
+    quality ~ .,
+    data = trainset,
+    method = "svmRadial", #svmLinear, svmPoly, svmRadial, svmRadialWeights
+    tuneGrid = grid_radial,
+    #tuneLength=10,
+    trControl = tr_control
+  )
+  end_time <- Sys.time()
+  time <- end_time - start_time
+
+  # Save the model
+  saveRDS(svm_model, file = "./results/models/svm_model.RDS")
+
+  # Return
+  list(model = svm_model, train_time = time)
+}
