5320239b6585cc234f14db45eb7de17c07eeafee
lordfighterx <lordfighterx@gmail.com>
2021-03-02 19:16:12 +0100

    Fix test.R

--- progetto/test.R
+++ progetto/test.R
@@ -14,13 +14,17 @@ source("./utils.R")
 source("models/dt.R")
 source("models/svm.R")
 
+# Prepare the dataset
+testset <- read.csv('./dataset/winequality-test.csv')
+testset$quality <- factor(testset$quality)
+
 # load models
 obj1 <- .get_model("dt")
 obj2 <- .get_model("svm")
 
 # Evaluate the model
-res1 <- evaluate_model(obj1$model, dataset, obj1$pre_proc)
-res2 <- evaluate_model(obj2$model, dataset, obj2$pre_proc)
+res1 <- evaluate_model(obj1$model, testset, obj1$pre_proc)
+res2 <- evaluate_model(obj2$model, testset, obj2$pre_proc)
 
 # Write Logs
 write_log("dt", res1$cm, res1$pred_time)
@@ -28,6 +32,6 @@ write_log("svm", res2$cm, res2$pred_time)
 
 # Plot AUCs ROC & PRC
 predictions <- list(dt_pred = res1$pred, svm_pred = res2$pred)
-plot_roc_and_prc_all(dataset, predictions)
+plot_roc_and_prc_all(testset, predictions)
 
 
--- progetto/train.R
+++ progetto/train.R
@@ -1,6 +1,25 @@
 #'
 #'
 #'
+#'
+
+.prepare_dataset <- function() {
+  # Prepare the dataset
+  dataset <- read.csv('./dataset/winequality-combined.csv')
+
+  # Setup quality
+  dataset$quality <- ifelse(dataset$quality > 6, 'good', 'bad')
+  dataset$quality <- factor(dataset$quality)
+  dataset$type <- NULL
+
+  # Create Partition
+  index <- createDataPartition(dataset$quality, p = 0.7, list = FALSE)
+  trainset <- dataset[index,]
+  testset <- dataset[-index,]
+
+  write.csv(trainset, "./dataset/winequality-train.csv", row.names = FALSE)
+  write.csv(testset, "./dataset/winequality-test.csv", row.names = FALSE)
+}
 
 # Install packages
 if (!require("pacman")) install.packages("pacman")
@@ -11,19 +30,11 @@ source("./utils.R")
 source("models/dt.R")
 source("models/svm.R")
 
+.prepare_dataset()
 
-# Prepare the dataset
-dataset <- read.csv('./dataset/winequality-combined.csv')
-
-# Setup quality
-dataset$quality <- ifelse(dataset$quality > 6, 'good', 'bad')
-dataset$quality <- factor(dataset$quality)
-dataset$type <- NULL
-
-# Create Partition
-index <- createDataPartition(dataset$quality, p = 0.75, list = FALSE)
-trainset <- dataset[index,]
-testset <- dataset[-index,]
+# Read trainset
+trainset <- read.csv('./dataset/winequality-train.csv')
+trainset$quality <- factor(trainset$quality)
 
 #Subsempling
 trainset <- subsempling(trainset, "SMOTE")
