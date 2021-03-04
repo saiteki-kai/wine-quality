c3badc7dbcd405909177451b850e9e53820d77a7
Giuseppe <giuseppe.magazu@gmail.com>
2021-03-04 01:28:39 +0100

    remove old models

--- progetto/train.R
+++ progetto/train.R
@@ -44,6 +44,10 @@ grid_radial <- expand.grid(
   sigma = 0.9, # c(0.1,0.8,0.9,1,1.1,1.2,1.3,1.4),
   C = 1.2 # c(0.1,0.8,0.9,1,1.1,1.2,1.3,1.4)
 )
+"grid_radial <- expand.grid(
+   sigma = c(0.01, 0.5, 1), #c(0,0.01,0.1,0.5,0.75,0.9,1),
+   C = c(0.01, 1, 2) #0,0.01,0.1,0.5,0.75,0.9,1,1.5,2,5)
+ )"
 grid_tree <- expand.grid(maxdepth = 2:10)
 
 # Train the models
