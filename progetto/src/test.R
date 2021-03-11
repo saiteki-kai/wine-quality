18bb26233f48dd9406402514bae7a7ec7fa3dec3
Giuseppe <giuseppe.magazu@gmail.com>
2021-03-11 14:03:28 +0100

    refactor

--- progetto/src/test.R
+++ progetto/src/test.R
@@ -2,11 +2,13 @@
 #'
 
 .get_model <- function(model_name, pre_proc_method) {
-  filename <- file.path("../output", paste0(model_name, "_", pre_proc_method, ".RDS"))
+  filename <- file.path(model_path, paste0(model_name, "_", pre_proc_method, ".RDS"))
 
   if (file.exists(filename)) {
     return(readRDS(filename))
   }
+
+  NULL
 }
 
 #' Write a log file
@@ -16,7 +18,7 @@
 #'
 .write_log <- function(model_name, pre_proc_name, cm, pred_time) {
   filename <- file.path(
-    "../output",
+    log_path,
     paste0(model_name, "_", pre_proc_name, ".log")
   )
 
@@ -47,33 +49,41 @@
 #' @param models a list of models
 #'
 #' @return AUCs for ROC and PRC for all the models
-.plot_roc_and_prc_all <- function(labels, probs, names, method) {
+.plot_roc_prc <- function(labels, probs, names, method) {
   labels <- rep(list(labels), length(names))
   labels <- join_labels(labels)
 
   scores <- join_scores(probs)
 
-  mmmdat <- mmdata(scores, labels,
+  sscurves <- evalmod(
+    mode = "rocprc",
+    scores = scores,
+    labels = labels,
     modnames = names,
     dsids = as.numeric(as.factor(names))
   )
-
-  ROC_PRC <- evalmod(mmmdat, mode = "rocprc")
 
   png(file.path("../plots/roc", paste0(method, ".png")),
       units = "in",
       res = 300,
       height = 6.67,
-      width = ifelse(TRUE, 13.34, 6.67))
+      width = ifelse(TRUE, 13.34, 6.67)
+  )
   # Plot ROC and PRC
-  p <- autoplot(ROC_PRC) + title(method)
+  autoplot(sscurves)
   dev.off()
 
-  # Calculate CI of AUCs with normal distibution
-  auc_ci <- auc_ci(ROC_PRC)
+  # Format AUC
+  aucs <- auc(sscurves) %>% dplyr::select("modnames", "curvetypes", "aucs")
+  aucs$curvetypes <- factor(aucs$curvetypes, levels = unique(aucs$curvetypes))
+  aucs <- aucs %>% split(aucs$curvetypes) %>%
+    lapply(function(x) {
+      x <- dplyr::select(x, "modnames", "aucs")
+      names(x) <- c("model", "AUC")
+      x
+    })
 
-  # Use knitr::kable to display the result in a table format
-  knitr::kable(auc_ci)
+  aucs
 }
 
 #' Print all the measures for the model evaluation
@@ -120,6 +130,10 @@ pacman::p_load(caret, ggplot2, grid, pROC, dplyr)
 # Local functions
 source("utils.R")
 
+log_path <- "../output"
+roc_path <- "../plots/roc"
+model_path <- "../output"
+
 # Prepare the dataset
 testset <- read.csv("../data/winequality-test.csv")
 testset$quality <- factor(testset$quality)
@@ -129,36 +143,36 @@ for (method in c("pca", "z-score", "min-max")) {
   rpart.model <- .get_model("rpart2", method)
   svmRadial.model <- .get_model("svmRadial", method)
   svmLinear.model <- .get_model("svmLinear", method)
-  #svmPoly.model <- .get_model("svmPoly", method)
+  # svmPoly.model <- .get_model("svmPoly", method)
 
   # Evaluate the model
   res1 <- .evaluate_model(rpart.model, testset)
   res2 <- .evaluate_model(svmRadial.model, testset)
   res3 <- .evaluate_model(svmLinear.model, testset)
-  #res4 <- .evaluate_model(svmPoly.model, testset)
+  # res4 <- .evaluate_model(svmPoly.model, testset)
 
   # Write Logs
   .write_log("rpart2", method, res1$cm, res1$pred_time)
   .write_log("svmRadial", method, res2$cm, res2$pred_time)
   .write_log("svmLinear", method, res3$cm, res3$pred_time)
-  #.write_log("svmPoly", method, res4$cm, res4$pred_time)
+  # .write_log("svmPoly", method, res4$cm, res4$pred_time)
 
   predictions <- list(
     rpart2 = res1$probs$good,
     svmRadial = res2$probs$good,
-    svmLinear = res3$probs$good#,
-    #svmPoly = res4$probs$good
+    svmLinear = res3$probs$good # ,
+    # svmPoly = res4$probs$good
   )
 
   names <- c(
     "rpart2",
     "svmRadial",
-    "svmLinear"#,
-    #"svmPoly"
+    "svmLinear" # ,
+    # "svmPoly"
   )
 
   # Plot AUCs ROC & PRC
-  roc_prc <- .plot_roc_and_prc_all(testset$quality, predictions, names, method)
+  roc_prc <- .plot_roc_prc(testset$quality, predictions, names, method)
 
   print(roc_prc)
 
@@ -167,30 +181,30 @@ for (method in c("pca", "z-score", "min-max")) {
     list(
       rpart2 = rpart.model,
       svmRadial = svmRadial.model,
-      svmLinear = svmLinear.model#,
-      #svmPoly = svmPoly.model
+      svmLinear = svmLinear.model # ,
+      # svmPoly = svmPoly.model
     )
   )
 
-  summary(cv.values)
-  print_or_save(dotplot(cv.values, metric = "ROC"),
-    "../plots/comparison/dotplot.png",
-    save = TRUE,
-    wide = TRUE
-  )
-  print_or_save(bwplot(cv.values, layout = c(3, 1)),
-    "../plots/comparison/bwplot.png",
-    save = TRUE,
-    wide = TRUE
-  )
-  print_or_save(splom(cv.values, metric = "ROC"),
-    "../plots/comparison/splom.png",
-    save = TRUE,
-    wide = TRUE
-  )
+  # summary(cv.values)
+  # print_or_save(dotplot(cv.values, metric = "ROC"),
+  #   "../plots/comparison/dotplot.png",
+  #   save = TRUE,
+  #   wide = TRUE
+  # )
+  # print_or_save(bwplot(cv.values, layout = c(3, 1)),
+  #   "../plots/comparison/bwplot.png",
+  #   save = TRUE,
+  #   wide = TRUE
+  # )
+  # print_or_save(splom(cv.values, metric = "ROC"),
+  #   "../plots/comparison/splom.png",
+  #   save = TRUE,
+  #   wide = TRUE
+  # )
 }
 
-#print(t_test(pred1-pred2, paired = TRUE))
+# print(t_test(pred1-pred2, paired = TRUE))
 
 # TODO: save plots and divide subsampled results from non subsampled
 # TODO: test statistici
