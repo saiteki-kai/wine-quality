587649fc28542ca6c63b5574b473c027504f5814
Giuseppe <giuseppe.magazu@gmail.com>
2021-05-11 15:09:47 +0200

    update tuning parameters

--- progetto/src/config.R
+++ progetto/src/config.R
@@ -32,20 +32,20 @@ comparison_path <- file.path("..", "plots", "comparison", subfolder)
 #
 # https://www.rdocumentation.org/packages/kernlab/versions/0.9-27/topics/dots
 
-C <- 2^c(0, 1, 2, 3) # c(0.1, 0.5, 1, 1.5, 2) # seq(0.25, 2, 0.25)
+C <- c(0.01, seq(0.1, 1.5, 0.1), 2, 5, 10)
+sigma <- c(0.01, seq(0.1, 1.5, 0.1), 2, 5, 10)
 
 grid_linear <- expand.grid(C = C)
-grid_radial <- expand.grid(sigma = c(0.01, 0.1, 0.5, 1), C = C)
-grid_poly <- expand.grid(degree = 1:3, scale = 1, C = C)
-grid_tree <- expand.grid(cp = c(0.001, 0.005, 0.01, 0.05, 0.1))
+grid_poly <- expand.grid(degree = 2:6, scale = 1, C = C)
+grid_radial <- expand.grid(sigma = sigma, C = C)
+grid_tree <- expand.grid(maxdepth = 2:30)
 
 # Models
 models <- list(
-  rpart2 = list(name = "rpart2", tune_grid = NULL, tune_length = 10),
-  svmLinear = list(name = "svmLinear", tune_grid = grid_linear),
+  rpart2 = list(name = "rpart2", tune_grid = grid_tree),
+  # svmLinear = list(name = "svmLinear", tune_grid = grid_linear),
+  # svmPoly = list(name = "svmPoly", tune_grid = grid_poly),
   svmRadial = list(name = "svmRadial", tune_grid = grid_radial)
-  # nb = list(name = "nb", tune_grid = NULL, tune_length = 3)
-  # svmPoly = list(name = "svmPoly", tune_grid = grid_poly)
   # knn = list(name = "knn", tune_grid = NULL, tune_length = 10)
 )
 
